37f4380fec5fbb4d77826f9c486b5994490895e5
Daniel Gomes <daniel.gomes@ubots.com.br>
2024-02-21 11:45:27 -0300

    cors

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -8,5 +8,14 @@ def create_app():
 
     define_routes(app)
     CORS(app, support_credentials=True)
+
+    @app.after_request
+    def cors(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers',
+                            'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+        return response
+
     
     return app
