29ad5fcc3fdeb8220610da78fb04312cc76f0477
Daniel Gomes <daniel.gomes@ubots.com.br>
2024-02-21 09:46:57 -0300

    corrige funções auxiliares

--- src/app/services/answer.py
+++ src/app/services/answer.py
@@ -3,22 +3,27 @@ import json
 import time
 import os
 
+ASSISTANT_ID = os.environ["ASSISTANT_ID"]
+
+FILES = {
+    "file_id": {"title": "TITULO", "link": "url.com.br"}
+}
+
 key = os.environ["OPENAI_KEY"]
+
 headers = {
       'Authorization': f'Bearer {key}',
       'OpenAI-Beta': 'assistants=v1',
       'Content-Type': 'application/json'
 }
 
-ASSISTANT_ID = os.environ["ASSISTANT_ID"]
-
-def create_thread(assistant_id=ASSISTANT_ID, headers=headers):
+def _create_thread(headers=headers):
     url = f'https://api.openai.com/v1/threads'
     response = requests.post(url, headers=headers)
     return response.json().get("id")
 
 
-def create_message(thread_id, question,  headers=headers):
+def _create_message(thread_id, question,  headers=headers):
     url = f'https://api.openai.com/v1/threads/{thread_id}/messages'
     payload = {
         "role": "user",
@@ -28,7 +33,7 @@ def create_message(thread_id, question,  headers=headers):
     return response.json().get("id")
 
 
-def create_run(thread_id, assistant_id=ASSISTANT_ID, headers=headers):
+def _create_run(thread_id, assistant_id=ASSISTANT_ID, headers=headers):
     url = f'https://api.openai.com/v1/threads/{thread_id}/runs'
     payload = {
         "assistant_id": assistant_id
@@ -37,25 +42,24 @@ def create_run(thread_id, assistant_id=ASSISTANT_ID, headers=headers):
     return response.json().get("id")
 
 
-def run_retrive(thread_id, run_id, headers=headers):
+def _run_retrive(thread_id, run_id, headers=headers):
     url = f'https://api.openai.com/v1/threads/{thread_id}/runs/{run_id}'
-    response = requests.get(url, headers=headers)
-    return response.json().get("id")
+    requests.get(url, headers=headers)
 
 
-def get_completion_assistant(thread_id, headers=headers):
+def _get_completion_assistant(thread_id, headers=headers):
     url = f'https://api.openai.com/v1/threads/{thread_id}/messages'
     response = requests.get(url, headers=headers)
     return response.json().get("data")[0].get("content")[0].get("text").get("value")
 
 
-def answer_question(question, headers=headers, time_sleep=60):
-    thread_id = create_thread()
-    create_message(thread_id, question)
-    run_id = create_run(thread_id)
-    run_retrive(thread_id, run_id)
+def answer_question(question, time_sleep=60):
+    thread_id = _create_thread()
+    _create_message(thread_id, question)
+    run_id = _create_run(thread_id)
+    _run_retrive(thread_id, run_id)
     time.sleep(time_sleep)
-    answer = get_completion_assistant(thread_id)
+    answer = _get_completion_assistant(thread_id)
 
     response = {"answer": answer}
 
